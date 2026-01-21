5927cb9dd51fae29bc90a6d9791b4980af914057
Pedro Henrique Napoli <pedrohenriquenapoli@gmail.com>
2026-01-21 17:40:36 -0300

    Initial commit

---
+++ R/RobustPanelQCA-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+# Declarações globais para evitar NOTEs no R CMD check
+utils::globalVariables(c(
+  ".data", ".X", ".Y", ".Xsol",
+  "inclN", "robust_share", "term"
+))
